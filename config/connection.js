3950556e640f891accd50eb9ebf59cebf9062272
Kajian Pulenthirasingam <kajianpulenthirasingam@gmail.com>
2023-06-16 04:14:10 +0200

    FIxing bugs

--- config/connection.js
+++ config/connection.js
@@ -1,5 +1,6 @@
 require('dotenv').config();
 const Sequelize = require('sequelize');
+
 let sequelize;
 
 if (process.env.JAWSDB_URL) {
@@ -8,7 +9,7 @@ if (process.env.JAWSDB_URL) {
   sequelize = new Sequelize(
     process.env.DB_NAME,
     process.env.DB_USER,
-    process.env.DB_PASS,
+    process.env.DB_PASSWORD,
     {
       host: 'localhost',
       dialect: 'mysql',
--- db/schema.sql
+++ db/schema.sql
@@ -1,3 +1,5 @@
 DROP DATABASE IF EXISTS tech_blog_db;
 CREATE DATABASE tech_blog_db;
 USE tech_blog_db;
+GRANT ALL PRIVILEGES ON *.* TO 'root'@'localhost' WITH GRANT OPTION;
+ALTER USER 'root'@'localhost' IDENTIFIED BY '3239';

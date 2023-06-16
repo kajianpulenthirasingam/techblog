83bc09da36cb18a3b37f0e240b733e8782813673
Kajian Pulenthirasingam <kajianpulenthirasingam@gmail.com>
2023-06-16 04:34:26 +0200

    bugs

--- server.js
+++ server.js
@@ -3,20 +3,16 @@ const express = require("express");
 const session = require("express-session");
 const exphbs = require("express-handlebars");
 const routes = require('./controllers/routes');
-
-
-
 const sequelize = require("./config/connection");
 const SequelizeStore = require("connect-session-sequelize")(session.Store);
 
 const app = express();
-const PORT = process.env.PORT || 3001;
+const PORT = process.env.PORT || 3000;
 
 const sess = {
   secret: "Super secret secret",
   cookie: {
-    // Stored in milliseconds
-    maxAge: 24 * 60 * 60 * 1000, // expires after 1 day
+    maxAge: 24 * 60 * 60 * 1000, // Expires after 1 day
   },
   resave: false,
   saveUninitialized: true,
@@ -25,21 +21,24 @@ const sess = {
   }),
 };
 
-app.use(session(sess));
+// Configure Handlebars.js engine
+const hbs = exphbs.create({});
 
-// Set up Handlebars.js engine with custom helpers
-
-
-// Inform Express.js on which template engine to use
-
+app.engine("handlebars", hbs.engine);
 app.set("view engine", "handlebars");
 
+// Middleware
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(express.static(path.join(__dirname, "public")));
+app.use(session(sess));
 
+// Routes
 app.use(routes);
 
+// Start the server
 sequelize.sync({ force: false }).then(() => {
-  app.listen(PORT, () => console.log("Now listening"));
+  app.listen(PORT, () => {
+    console.log(`Server is listening on port ${PORT}`);
+  });
 });
